542348a265866792525876558c62ca52e8f70f88
Yoan <yoanshimoni@gmail.com>
2020-12-05 11:26:02 +0200

    fixed port

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => console.log("Listening on port 3000"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
